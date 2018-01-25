9e4a0db14b52e0dd8cc48352faa1bf4cb5f4906c
PhuongTran <tranduyphuong2110@gmail.com>
2018-01-25 03:04:41 -0800

    cannot load graph.js

--- sources/src/main/java/app/Main.java
+++ sources/src/main/java/app/Main.java
@@ -2,6 +2,7 @@ package app;
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.*;
 
+import app.home.HomeController;
 import app.signin.SigninController;
 import app.signup.SignupController;
 import app.util.Filters;
@@ -17,12 +18,13 @@ public class Main {
 		staticFiles.location("/public");
 		
 		// Set up before-filters (called before each get/post)
-		before("*", Filters.addTrailingSlashes);
+		//before("*", Filters.addTrailingSlashes);
 		
 		// Set up routes
-		get("/", (req,res) -> {
+		get("/hello/", (req,res) -> {
 			return "Hello world";
 		});
+		get(Path.Web.HOME, 			HomeController.handleHomeDisplay);
 		get(Path.Web.SIGNIN, 		SigninController.handleSigninDisplay);
 		get(Path.Web.SIGNUP, 		SignupController.handleSignupDisplay);
 		get("*", 					ViewUtil.notFound);
--- sources/src/main/java/app/home/HomeController.java
+++ sources/src/main/java/app/home/HomeController.java
@@ -1,5 +1,16 @@
 package app.home;
+import java.util.HashMap;
+import java.util.Map;
+
+import app.util.Path;
+import app.util.ViewUtil;
+import spark.*;
 
 public class HomeController {
-
+	private static final String apiKey = "LSHfJJyvzYHUyU9jHpn6"; 
+	public static Route handleHomeDisplay = (Request request, Response response) -> {
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		return ViewUtil.render(request, model, Path.Templates.HOME);
+	};
 }
