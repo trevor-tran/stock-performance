1815aea6ec0282528a39e1f8dc9b518dd5f4a1f8
PhuongTran <tranduyphuong2110@gmail.com>
2018-01-28 05:27:01 -0800

    functions add  new users, dealing with database

--- sources/src/main/java/app/user/UserController.java
+++ sources/src/main/java/app/user/UserController.java
@@ -7,12 +7,25 @@ public class UserController {
 		if( username.isEmpty() || password.isEmpty() ){
 			return false;
 		}
-		User user = UserDao.getUserByName(username);
+		User user = UserDao.getUserCredentials(username);
 		if(user == null){
 			return false;
 		}
 		String hashedPassword = BCrypt.hashpw(password, user.getSalt());
 		return hashedPassword.equals(user.getHashedPassword());
 	}
-	
+	/**
+	 * Hash password and create a new user in database
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param username
+	 * @param password
+	 * @throws Exception
+	 */
+	public static void createUser(String firstName,String lastName,String email, String username,String password) throws Exception{
+		String salt = BCrypt.gensalt();
+		String hashedPassword = BCrypt.hashpw(password, salt);
+		UserDao.setUser(firstName, lastName, email, username, salt, hashedPassword);
+	}
 }
--- sources/src/main/java/app/user/UserDao.java
+++ sources/src/main/java/app/user/UserDao.java
@@ -3,6 +3,7 @@ package app.user;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 
@@ -13,24 +14,18 @@ import app.util.DatabaseConnection;;
  *
  */
 public class UserDao {
-	private static Statement connect() throws Exception
-	{
-		Connection connection = DatabaseConnection.getConnection();
-		Statement statement = connection.createStatement();
-		return statement;
-
-	}
+	
 	/**
 	 * Get all credentials of the user from database
 	 * @param username
 	 * @return User
 	 * @throws Exception
 	 */
-	public static User getUserByName(String username) throws Exception{
-		Statement statement = connect();
+	public static User getUserCredentials(String username) throws Exception{
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = connection.createStatement();
 		String sql = String.format("SELECT * FROM users WHERE username='%s'",username);
 		ResultSet rs = statement.executeQuery(sql);
-		//DatabaseConnection.closeConnection(); TODO:why cannot close connection at this point?
 		if(rs.next()){
 			//first column is user_id
 			String firstName = rs.getString(2);
@@ -43,10 +38,54 @@ public class UserDao {
 			Date startDate = rs.getDate(9);
 			Date endDate = rs.getDate(10);
 			User user = new User(firstName,lastName,username, salt, hashedPassword, email, investment, startDate, endDate);
+			close(connection,statement,rs);
 			return user;
 		}
+		close(connection,statement,rs);
 		return null;
 	}
-	//public static void setUser(String username,)
+	/**
+	 * execute INSERT query to add new user to users_table
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param username
+	 * @param salt
+	 * @param hashedPassword
+	 * @throws Exception
+	 */
+	public static void setUser(String firstName,String lastName,String email, String username,String salt,String hashedPassword) throws Exception{
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = connection.createStatement();
+		String sql = String.format("INSERT INTO users(first_name,last_name,email,username,salt,hashed_password) "
+				+ "VALUES('%s','%s','%s','%s','%s','%s')", firstName,lastName,email,username,salt,hashedPassword);
+		statement.executeUpdate(sql);
+		close(connection,statement);
+	}
+	
+	/**
+	 * close <i>Connection,Statement, ResultSet</i>
+	 * @param connection
+	 * @param statement
+	 * @param rs
+	 * @throws SQLException
+	 */
+	private static void close(Connection connection, Statement statement, ResultSet rs) throws SQLException{
+		if(rs != null){rs.close();}
+		if( statement != null){ statement.close();}
+		if (connection != null){connection.close();}
+	}
+	/**
+	 * close <i>Connection, Statement</i>
+	 * @param connection
+	 * @param statement
+	 * @throws SQLException
+	 */
+	private static void close(Connection connection, Statement statement) throws SQLException{
+		if( statement != null){ statement.close();}
+		if (connection != null){connection.close();}
+	}
+
+
 
 }
