69d6d7eeb791421c07161857d3d87ec2e4e29428
PhuongTran <tranduyphuong2110@gmail.com>
2018-01-31 15:49:10 -0800

    renamed, deleted some private fields

--- sources/src/main/java/app/user/UserInfo.java
+++ sources/src/main/java/app/user/UserInfo.java
@@ -4,16 +4,12 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.util.Date;
 @Value // All fields are private and final. Getters (but not setters) are generated (https://projectlombok.org/features/Value.html)
-public class User {
-	String firstName;
-	String lastName;
-	String username;
-	String salt;
-	String hashedPassword;
-	String email;
+public class UserInfo {
 	BigDecimal investment;
 	Date startDate;
 	Date endDate;
+	String stockSymbol;
+	double numberOfShares;
 	
 	
 }
