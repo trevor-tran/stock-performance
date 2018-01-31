ab14564cc17784d753a51833930b4579a2265d34
PhuongTran <tranduyphuong2110@gmail.com>
2018-01-31 15:55:13 -0800

    get userId by userName, get other info by userId

--- sources/src/main/java/app/user/UserDao.java
+++ sources/src/main/java/app/user/UserDao.java
@@ -14,35 +14,70 @@ import app.util.DatabaseConnection;;
  *
  */
 public class UserDao {
-	
-	/**
-	 * Get all credentials of the user from database
-	 * @param username
-	 * @return User
-	 * @throws Exception
-	 */
-	public static User getUserCredentials(String username) throws Exception{
+
+	public static String getUserFirstName(int userId) throws Exception{
 		Connection connection = DatabaseConnection.getConnection();
 		Statement statement = connection.createStatement();
-		String sql = String.format("SELECT * FROM users WHERE username='%s'",username);
+		String sql = String.format("SELECT first_name FROM users WHERE user_id=%d" , userId);
+		ResultSet rs = statement.executeQuery(sql);
+		rs.next();
+		String firstName = rs.getString(1);
+		close(connection,statement,rs);
+		return firstName;
+	}
+
+	/**
+	 * Find user by username
+	 * @param username
+	 * @return <i>user_id</i> if found the user, <i>-1</i> if not found.
+	 * @throws Exception
+	 */
+	public static int getUserId(String username) throws Exception {
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = connection.createStatement();
+		String sql = String.format("SELECT user_id FROM users WHERE username='%s'" , username);
 		ResultSet rs = statement.executeQuery(sql);
 		if(rs.next()){
-			//first column is user_id
-			String firstName = rs.getString(2);
-			String lastName = rs.getString(3);
-			//fourth column is username
-			String salt = rs.getString(5);
-			String hashedPassword = rs.getString(6);
-			String email = rs.getString(7);
-			BigDecimal investment = rs.getBigDecimal(8);
-			Date startDate = rs.getDate(9);
-			Date endDate = rs.getDate(10);
-			User user = new User(firstName,lastName,username, salt, hashedPassword, email, investment, startDate, endDate);
+			int userId = rs.getInt(1);
 			close(connection,statement,rs);
-			return user;
+			return userId;
 		}
 		close(connection,statement,rs);
-		return null;
+		return -1 ;
+	}
+
+	/**
+	 * Get sign-in credentials, salt and hashed password
+	 * @param userId
+	 * @return <i>SigninCredentials</i>, <i>null</i> if userId not exist
+	 * @throws Exception
+	 */
+	public static SigninCredentials getSigninCredentials(int userId) throws Exception{
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = connection.createStatement();
+		String sql = String.format("SELECT salt,hashed_password FROM users WHERE user_id=%d",userId);
+		ResultSet rs = statement.executeQuery(sql);
+		rs.next();
+		String salt = rs.getString(1);
+		String hashedPassword = rs.getString(2);
+		close(connection,statement,rs);
+		return new SigninCredentials(salt, hashedPassword);
+	}
+	public static UserInfo getUserInvestmentInfo( int userId) throws Exception{
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = connection.createStatement();
+		String sql = String.format("SELECT user.first_name,user.investment,user.start_date,user.end_date,stocks.symbol,stock.number_of_shares"
+				+ " FROM users WHERE users.user_id=%d"
+				+ " AND users.user_id=stocks.user_id",userId);
+		ResultSet rs = statement.executeQuery(sql);
+		rs.next();
+		BigDecimal investment = rs.getBigDecimal(1);
+		Date startDate = rs.getDate(2);
+		Date endDate = rs.getDate(3);
+		String stockSymbol = rs.getString(4);
+		double numberOfShares = rs.getInt(5);
+		close(connection,statement,rs);
+		return new UserInfo(investment, startDate, endDate, stockSymbol, numberOfShares);
 	}
 	/**
 	 * execute INSERT query to add new user to users_table
@@ -62,7 +97,7 @@ public class UserDao {
 		statement.executeUpdate(sql);
 		close(connection,statement);
 	}
-	
+
 	/**
 	 * close <i>Connection,Statement, ResultSet</i>
 	 * @param connection
