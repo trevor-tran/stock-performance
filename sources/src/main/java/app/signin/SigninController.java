865e00d28cf08ad31802b6587cd5d33b8d5a3ed5
PhuongTran <tranduyphuong2110@gmail.com>
2018-01-31 15:57:15 -0800

    updated corresponding to userDao.java; handle signOut

--- sources/src/main/java/app/signin/SigninController.java
+++ sources/src/main/java/app/signin/SigninController.java
@@ -1,13 +1,14 @@
 package app.signin;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import app.util.Path;
 import app.util.ViewUtil;
 import spark.*;
-import app.user.UserController;
+import static app.user.UserController.*;
 public class SigninController {
-	
+
 	public static Route handleSigninDisplay = (Request request, Response response) -> {
 		Map<String,Object> model = new HashMap<String,Object>();
 		return ViewUtil.render(request, model, Path.Templates.SIGNIN);
@@ -20,15 +21,37 @@ public class SigninController {
 			model.put("usernameContainsSpace", true);
 			return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 		}
-		if(UserController.authenticate(username, password)){
-			model.put("authenticationSucceeded", true);
-			response.redirect(Path.Web.HOME);
+		int userId = authenticate(username, password);
+		if(userId != -1){
+			model.put("firstName", getFirstName(userId));
+			request.session().attribute("userId", userId);
+			model.put("userId", userId);
+			return ViewUtil.render(request, model, Path.Templates.HOME);
 		}
 		else{
 			model.put("authenticationFailed", true);
 			return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 		}
-		
-		return null;//ViewUtil.render(request, model, Path.Templates.SIGNIN);
+
+		//return null;//ViewUtil.render(request, model, Path.Templates.SIGNIN);
+	};
+	public static Route handleSignoutPost = (Request request, Response response) -> {
+		request.session().removeAttribute("userId");
+		request.session().attribute("SignedOut", true);
+		response.redirect(Path.Web.HOME);
+		return null;
+	};
+
+	public static boolean IsSignIn(Request request, Response response) throws Exception {
+		if (request.session().attribute("userId") == null) {
+			// the current username/email may be passed to us in a cookie
+			//String userCookie = request.cookie("currentUser");
+			//if (userCookie != null && userCookie.length() > 0) {
+			// save the username in a session variable with the same key name
+			//request.session().attribute("currentUser", userCookie);
+			//handleGoogleSignIn(userCookie);
+			return false;
+		}
+		return true;
 	};
 }
