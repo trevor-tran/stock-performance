6e47518b460e03a2a45411a87e86f24ef6b99d33
PhuongTran <tranduyphuong2110@gmail.com>
2018-02-08 22:52:17 -0800

    add url where to get json data

--- sources/src/main/java/app/StockApplication.java
+++ sources/src/main/java/app/StockApplication.java
@@ -12,29 +12,34 @@ import app.util.ViewUtil;
 public class StockApplication {
 
 	public static void main(String[] args) {
-		
+
 		DatabaseConnection.initialize();
-		
+
 		//configure Spark
 		port(4567);
 		enableDebugScreen();
 		staticFiles.location("/public");
-		
+
 		// Set up before-filters (called before each get/post)
 		before("*", Filters.addTrailingSlashes);
-		
+
 		// Set up routes
 		get("/hello/", (req,res) -> {
 			return "Hello world";
 		});
-		get(Path.Web.HOME, 			HomeController.handleHomeDisplay);
+		//get(Path.Web.HOME, 			HomeController.handleHomeDisplay);
+		//get(Path.Web.HOME,         HomeController.fetchAllStocks);
+		get(Path.Web.STOCKDATA, "application/json", HomeController.fetchRawStockData);
 		get(Path.Web.SIGNIN, 		SigninController.handleSigninDisplay);
 		post(Path.Web.SIGNIN, 		SigninController.handelSigninPost);
 		post(Path.Web.SIGNOUT, 		SigninController.handleSignoutPost);
 		get(Path.Web.SIGNUP, 		SignupController.handleSignupDisplay);
 		post(Path.Web.SIGNUP, 		SignupController.handleSignupPost);
+		
 		get("*", 					ViewUtil.notFound);
 		
+		after(Path.Web.HOME,       Filters.addGzipHeader);
+
 	}
 
 }
