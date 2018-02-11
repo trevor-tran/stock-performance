bb1089cfda8cd0a6c062f853c52308db4d0acd80
PhuongTran <tranduyphuong2110@gmail.com>
2018-02-10 22:18:51 -0800

    set userId and firstName as session attributes if signed in, delete these if signed out

--- sources/src/main/java/app/signin/SigninController.java
+++ sources/src/main/java/app/signin/SigninController.java
@@ -19,31 +19,33 @@ public class SigninController {
 		String password = request.queryParams("password");
 		if ( username.contains(" ")){
 			model.put("usernameContainsSpace", true);
-			return ViewUtil.render(request, model, Path.Templates.SIGNIN);
+			//return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 		}
 		int userId = authenticate(username, password);
-		if(userId != -1){
-			model.put("firstName", getFirstName(userId));
-			request.session().attribute("userId", userId);
-			model.put("userId", userId);
-			return ViewUtil.render(request, model, Path.Templates.HOME);
+		if(userId == -1){
+			model.put("authenticationFailed", true);
+			//return ViewUtil.render(request, model, Path.Templates.SIGNIN);
+			
 		}
 		else{
-			model.put("authenticationFailed", true);
-			return ViewUtil.render(request, model, Path.Templates.SIGNIN);
+			request.session().attribute("firstName", getFirstName(userId));
+			request.session().attribute("currentUserId", userId);
+			response.redirect(Path.Web.HOME);
+			//return ViewUtil.render(request, model, Path.Templates.HOME);
 		}
-
+		return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 		//return null;//ViewUtil.render(request, model, Path.Templates.SIGNIN);
 	};
 	public static Route handleSignoutPost = (Request request, Response response) -> {
-		request.session().removeAttribute("userId");
-		request.session().attribute("SignedOut", true);
+		request.session().removeAttribute("currentUserId");
+		request.session().removeAttribute("firstName");
+		//request.session().attribute("SignedOut", true);
 		response.redirect(Path.Web.HOME);
 		return null;
 	};
 
 	public static boolean IsSignIn(Request request, Response response) throws Exception {
-		if (request.session().attribute("userId") == null) {
+		if (request.session().attribute("currentUserId") == null) {
 			// the current username/email may be passed to us in a cookie
 			//String userCookie = request.cookie("currentUser");
 			//if (userCookie != null && userCookie.length() > 0) {
