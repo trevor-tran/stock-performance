0227c535700e06d099b720853fbdea76ba1491fe
PhuongTran <tranduyphuong2110@gmail.com>
2018-02-16 23:12:51 -0800

    test failed. got error due to css loader.

--- sources/webpack/src/js/components/container/DateChooser.js
+++ sources/webpack/src/js/components/container/DateChooser.js
@@ -1,14 +1,16 @@
-imprt React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import ReactDOM from 'react-dom';
-import 'react-datepicker/dist/react-datepicker.css';
+
+
+//import 'react-datepicker/dist/react-datepicker.css';
 
 class DateChooser extends React.Component {
 	constructor (props) {
-		super(props)
+		super(props);
 		this.state = {
-			startDate: moment();
+			startDate: moment()
 			//endDate: moment();
 		};
 		this.handleChange = this.handleChange.bind(this);
@@ -20,7 +22,7 @@ class DateChooser extends React.Component {
 		return <DatePicker selected = {this.state.startDate} onChange={this.handleChange} />;
 	}
 }
-const dateWraper = document.getElementById("startdate");
-dateWrapper ? ReactDOM.render(<Datechooser/>, dateWrapper) : false;
+const dateWrapper = document.getElementById("startdate");
+dateWrapper ? ReactDOM.render(<DateChooser/>, dateWrapper) : false;
 
 export default DateChooser;
