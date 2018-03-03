ddc8ea209b1faec985192f2d145c0570e345a197
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-02 20:55:09 -0800

    a spinner

---
+++ sources/webpack/src/js/components/presentational/Spinner.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import $ from 'jquery';
+import PropTypes from 'react';
+import {GridLoader} from 'halogenium';
+
+class Spinner extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		const name = this.props.setClass;
+		const overlay = {
+			    background: "#e9e9e9",
+				display: "none",
+				position: "absolute",
+				top: 0,
+	    		right: 0,
+	    		bottom: 0,
+	    		left: 0,
+			    opacity: 0.5,
+		};
+		
+		const img = {
+				position: "fixed",
+				zIndex:20,
+				top: "50%",
+				left: "50%",
+				transform: "translate(-50%, -50%)"
+		};
+		return(
+			<div className={name} style={overlay}>
+				<GridLoader style={img} color="#26A65B" size="20px" margin="10px"/>
+			</div>
+		);
+	}
+}
+export default Spinner;
