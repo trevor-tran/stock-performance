fdabcfe01a9bad071af646a07d6c966446281d1b
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-04 23:40:00 -0800

    get symbols from this.state to avoid the case missing symbols in first obj of array

--- sources/webpack/src/js/components/presentational/Graph.js
+++ sources/webpack/src/js/components/presentational/Graph.js
@@ -12,18 +12,14 @@ class Graph extends Component {
 		//properties
 		const data = this.props.data;
 		const name = this.props.setClass;
+		const symbols = this.props.getSymbols;
 		let lines = [];
 		if (data != null){
 			var colors = ['#8884d8', '#82ca9d', '#595163', '#8b2412','#f83581','#f07b50','#0c5e59','#0011ff','#e57cf9'];
 			var colorIndex = 0;
-			var obj1 = data[0]; // extract a object.
-			// loop over obj1, get keys,but "date".Add <Line> for each key, which is stock symbol.
-			$.each(obj1, function(key, value) {
-				if(key!="date") {
-					var color = colors [ colorIndex++ % colors.length]; // rotate colors
-					lines.push(<Line type="monotone" key={key} dataKey={key} dot={false} unit=" USD" stroke={color}/>);
-				}
-
+			symbols.forEach( function(symbol) {
+				var color = colors [ colorIndex++ % colors.length]; // rotate colors
+				lines.push(<Line type="monotone" key={symbol} dataKey={symbol} dot={false} unit=" USD" stroke={color}/>);
 			});
 					return(
 						<div className = {name}>
