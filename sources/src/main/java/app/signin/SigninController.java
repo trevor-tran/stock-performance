5656a3cfd7c2081ed7d1df1900d8a19b1c8da7bd
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-05 19:34:49 -0800

    organized imports, increase readability

--- sources/src/main/java/app/signin/SigninController.java
+++ sources/src/main/java/app/signin/SigninController.java
@@ -1,50 +1,50 @@
 package app.signin;
 
+import static app.user.UserController.authenticate;
+import static app.user.UserController.getFirstName;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import app.util.Path;
 import app.util.ViewUtil;
-import spark.*;
-import static app.user.UserController.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class SigninController {
 
 	public static Route handleSigninDisplay = (Request request, Response response) -> {
 		Map<String,Object> model = new HashMap<String,Object>();
 		return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 	};
+
 	public static Route handelSigninPost = (Request request, Response response) -> {
 		Map<String,Object> model = new HashMap<String,Object>();
 		String username = request.queryParams("username");
 		String password = request.queryParams("password");
 		if ( username.contains(" ")){
 			model.put("usernameContainsSpace", true);
-			//return ViewUtil.render(request, model, Path.Templates.SIGNIN);
 		}
 		int userId = authenticate(username, password);
 		if(userId == -1){
 			model.put("authenticationFailed", true);
-			//return ViewUtil.render(request, model, Path.Templates.SIGNIN);
-			
-		}
-		else{
+		}else{
 			request.session().attribute("firstName", getFirstName(userId));
 			request.session().attribute("currentUserId", userId);
 			response.redirect(Path.Web.HOME);
-			//return ViewUtil.render(request, model, Path.Templates.HOME);
 		}
 		return ViewUtil.render(request, model, Path.Templates.SIGNIN);
-		//return null;//ViewUtil.render(request, model, Path.Templates.SIGNIN);
 	};
+
 	public static Route handleSignoutPost = (Request request, Response response) -> {
 		request.session().removeAttribute("currentUserId");
 		request.session().removeAttribute("firstName");
-		//request.session().attribute("SignedOut", true);
 		response.redirect(Path.Web.HOME);
 		return null;
 	};
-
-	public static boolean IsSignIn(Request request, Response response) throws Exception {
+	//TODO: will use later to guarantee signed in before query user data
+	public static boolean isSignIn(Request request, Response response) throws Exception {
 		if (request.session().attribute("currentUserId") == null) {
 			// the current username/email may be passed to us in a cookie
 			//String userCookie = request.cookie("currentUser");
