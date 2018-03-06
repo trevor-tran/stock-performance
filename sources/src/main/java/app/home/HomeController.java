e0bfad2f649859240ced6a450cc5317572f086ae
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-05 18:19:44 -0800

    organized imports, unnessessary code delelted

--- sources/src/main/java/app/home/HomeController.java
+++ sources/src/main/java/app/home/HomeController.java
@@ -6,12 +6,12 @@ import static app.util.RequestUtil.clientAcceptsJson;
 import java.util.HashMap;
 import java.util.Map;
 
-
 import app.stock.StockDao;
-import app.util.JsonUtil;
 import app.util.Path;
 import app.util.ViewUtil;
-import spark.*;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 public class HomeController {
 
@@ -23,36 +23,16 @@ public class HomeController {
 			//model.put("stocks", StockDao.getAllStocks());
 			return ViewUtil.render(request, model, Path.Templates.HOME);
 		}
-		if (clientAcceptsJson(request)) {
-			Map<String,Map<String,Float>> data;
+		else if (clientAcceptsJson(request)) {
 			//String money = request.queryParams("money");
 			String start = request.queryParams("start");
 			String end = request.queryParams("end");
 			String symbol = request.queryParams("symbol");
-			data = StockDao.getStockData(symbol, start, end);
-			response.header("Content-Type", "application/json");
-			return dataToJson(data);
-		}
-		
-		return ViewUtil.notAcceptable.handle(request, response);
-	};
-
-	public static Route fetchRawStockData = (Request request, Response response) -> {
-		// TODO: validate JS session!
-		//LoginController.ensureUserIsLoggedIn(request, response);
-		if (clientAcceptsJson(request)) {
-
-			String symbol =",";
-			symbol += request.queryParams("symbol");
-			Map<String,Map<String,Float>> data = StockDao.getStockData("AAPL"+symbol, "2017-1-3", "2018-2-14");
-
+			Map<String,Map<String,Float>> data = StockDao.getStockData(symbol, start, end);
+			//TODO: handle null data
 			response.header("Content-Type", "application/json");
 			return dataToJson(data);
 		}
 		return ViewUtil.notAcceptable.handle(request, response);
 	};
-	/* public static Route handleStockRequest = (Request request, Response response) -> {
-		 Map<String,Object> model = new HashMap<String,Object>();
-
-	   };*/
 }
