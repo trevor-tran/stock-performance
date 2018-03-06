a45c41bf746d09ae4b5a454224f3a0afa35589c3
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-05 22:55:09 -0800

    no use

--- sources/src/main/java/app/stock/Stock.java
+++
@@ -1,9 +0,0 @@
-package app.stock;
-
-import lombok.*;
-
-@Value // All fields are private and final. Getters (but not setters) are generated (https://projectlombok.org/features/Value.html)
-public class Stock {
-    String name;
-    String symbol;
-}
