59b2dd5c73e021262b6ca3b5127beae98e036125
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-06 21:49:54 -0800

    invalid_user as constant. changes based on updated Password class

--- sources/src/main/java/app/user/UserController.java
+++ sources/src/main/java/app/user/UserController.java
@@ -1,36 +1,56 @@
 package app.user;
-import org.mindrot.jbcrypt.*;
 
 public class UserController {
-	public static boolean usernameExists( String username) throws Exception{
-		return (UserDao.getUserId(username) != -1) ? true : false;
-	}
 	
-	public static String getFirstName(int userId) throws Exception{
-		//TODO: possible to have invalid userId,maybe need "if"
-		return UserDao.getUserFirstName(userId);
+	public static final int INVALID_USER_ID = -1;
+	
+	public static boolean usernameExists( String username){
+		try{
+			return UserDao.getUserId(username) != -1;
+		}catch(Exception ex){
+			System.out.println("Error in usernameExists() method");
+			System.out.println(ex.getMessage());
+			return false;
+		}
 	}
+
+	public static String getFirstName(int userId){
+		try{
+			//TODO: possible to have invalid userId,maybe need "if"
+			return UserDao.getUserFirstName(userId);
+		}catch(Exception ex){
+			System.out.println("Error in getFirstName() method");
+			System.out.println(ex.getMessage());
+			return null;
+		}
+	}
+
 	/**
-	 * 
 	 * @param username
 	 * @param password
 	 * @return <i>-1</i> if authentication failed, <i>user_id</i> if succeeded
 	 * @throws Exception
 	 */
-	public static int authenticate(String username, String password) throws Exception{
-		if( username.isEmpty() || password.isEmpty() ){
-			return -1;
-		}
-		int userId = UserDao.getUserId(username);
-		if(userId == -1){
-			return -1;
-		}
-		else{
-			Password signinInfo = UserDao.getSigninCredentials(userId);
-			String hashedEnterPassword = BCrypt.hashpw(password, signinInfo.getSalt());
-			return (hashedEnterPassword.equals(signinInfo.getHashedPassword())) ? userId : -1; 
+	public static int authenticate(String username, String password){
+		try{
+			if( username.isEmpty() || password.isEmpty() ){
+				return INVALID_USER_ID;
+			}
+			int userId = UserDao.getUserId(username);
+			if(userId == -1){
+				return INVALID_USER_ID;
+			}
+			else{
+				Password signingInUser = UserDao.getSigninCredentials(userId);
+				return signingInUser.matches(password) ? userId : INVALID_USER_ID;
+			}
+		}catch(Exception ex){
+			System.out.println("Error in authenticate() method");
+			System.out.println(ex.getMessage());
+			return INVALID_USER_ID;
 		}
 	}
+
 	/**
 	 * Hash password and create a new user in database
 	 * @param firstName
@@ -40,9 +60,13 @@ public class UserController {
 	 * @param password
 	 * @throws Exception
 	 */
-	public static void createUser(String firstName,String lastName,String email, String username,String password) throws Exception{
-		String salt = BCrypt.gensalt();
-		String hashedPassword = BCrypt.hashpw(password, salt);
-		UserDao.setUser(firstName, lastName, email, username, salt, hashedPassword);
+	public static void createUser(String firstName,String lastName,String email, String username,String password){
+		try{
+			Password newPassword = new Password(password);
+			UserDao.setUser(firstName, lastName, email, username, newPassword.getSalt(), newPassword.getHashedPassword());;
+		}catch(Exception ex){
+			System.out.println("Error in  createUser() method");
+			System.out.println(ex.getMessage());
+		}
 	}
 }
