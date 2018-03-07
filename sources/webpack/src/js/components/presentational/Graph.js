2ff809a29122be3bd2c1c8534b80a8864204f0d6
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-07 14:58:56 -0800

    increased height

--- sources/webpack/src/js/components/presentational/Graph.js
+++ sources/webpack/src/js/components/presentational/Graph.js
@@ -24,7 +24,7 @@ class Graph extends Component {
 						<div className = {name}>
 							<ResponsiveContainer  width='100%' height='100%'>
 							<LineChart data={data}>
-							<XAxis dataKey="date" angle={-20} textAnchor="end" height={50} />
+							<XAxis dataKey="date" angle={-20} textAnchor="end" height={60} />
 							<YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }} />
 							<CartesianGrid strokeDasharray="3 3"/>
 								<Tooltip/>
