dd9f8ca036e70d2523dbc0eb4558adcfa6bebb29
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-11 03:31:39 -0700

    UI re-designed

--- sources/webpack/src/js/components/presentational/Summary.js
+++ sources/webpack/src/js/components/presentational/Summary.js
@@ -17,41 +17,29 @@ class Table extends Component{
 	}
 	render(){
 		//properties
-		const _self = this.props.getThis;
 		const name = this.props.setClassName;
-		if(_self.state.data.length != 0){
-			const invest = _self.state.invest;
-			const firstObj = _self.state.data[0];
-			const lastObj = _self.state.data[_self.state.data.length-1];
-			var rows=[];
-			return(
-				<table className={name} border="1" cellpadding="5"><tbody>
+		return(
+			<table className={name} border="1" cellpadding="5">
+				<theader>
 					<tr>
-						<th rowSpan="2">Stock Symbol</th>
-						<th>Balance</th>
-						<th>Balance</th>
+						<th rowSpan="2">Symbol</th>
+						<th colSpan="2">From 1/1/2013</th>
+						<th colSpan="3">To 1/1/2016</th>
 						<th rowSpan="2">Gain/Loss</th>
 						<th rowSpan="2">Difference</th>
 					</tr>
 					<tr>
-						<th>As {firstObj.date}</th>
-						<th>As {lastObj.date}</th>
+						<th>Price</th>
+						<th>Quantity</th>
+						<th>Price</th>
+						<th>Quantity</th>
+						<th>Balance</th>
 					</tr>
-					{ _self.state.symbols.map((symbol)=>(
-						<tr key={symbol}>
-							<td>{symbol}</td>
-							<td>${invest}</td>
-							<td>${lastObj[symbol]}</td>
-							<td>${roundUp(lastObj[symbol]-invest, 2)}</td>
-							<td></td>
-						</tr>
-						
-					))}
-				</tbody></table>
-			);
-		}else{
-			return false;
-		}
+				</theader>
+				<tbody>
+				</tbody>
+			</table>
+		);
 	}
 }
 
