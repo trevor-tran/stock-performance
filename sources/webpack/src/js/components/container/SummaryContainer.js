efbcad73c4d0c165089cd4f68126abecffdf0e40
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-12 15:35:27 -0700

    initial attempt on fetching summary. got null summary

--- sources/webpack/src/js/components/container/SummaryContainer.js
+++ sources/webpack/src/js/components/container/SummaryContainer.js
@@ -9,10 +9,48 @@ import async from 'async';
 
 import Summary from "../presentational/Summary";
 
+function fetchData() {
+	return new Promise(function(resolve, reject) {
+		//request json from server
+		fetch("/summary/", { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } })
+		.then(function(response) {
+			// convert to JSON
+			return response.json();
+		})
+		.then( function(newData){
+			resolve(newData);
+		})
+		.catch(function(error) {
+			console.log(error);
+			reject(error);
+		});
+	});
+}
+
 class SummaryContainer extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+				data: []
+		}
+	}
+	
+	componentWillMount(){
+		var _self = this;
+		fetchData().then(function(data){
+			console.log("type of summary:", typeof data);
+			console.log(data);
+			_self.setState((prevState) => {
+				return{data: prevState.data.push(data) };
+			});
+			console.log(this.state.data);
+		});
+	}
+	
 	render(){
 		return(
-			<Summary setClassName={this.props.setClassName}
+			<Summary 
+				setClassName={this.props.setClassName}
 			/>
 		);
 	}
--- sources/webpack/src/js/components/presentational/Summary.js
+++ sources/webpack/src/js/components/presentational/Summary.js
@@ -11,34 +11,48 @@ function roundUp(num, precision) {
   return Math.ceil(num * precision) / precision
 }
 
-class Table extends Component{
+class Summary extends Component{
 	constructor(props){
 		super(props);
 	}
 	render(){
 		//properties
 		const name = this.props.setClassName;
+		{/*const data = this.props.getData;*/}
 		return(
 			<table className={name} border="1" style={{cellpadding:"5"}}>
-				<tbody>
-					<tr>
-						<th rowSpan="2">Symbol</th>
-						<th colSpan="2">From 1/1/2013</th>
-						<th colSpan="3">To 1/1/2016</th>
-						<th rowSpan="2">Gain/Loss</th>
-						<th rowSpan="2">Difference</th>
-					</tr>
-					<tr>
-						<th>Price</th>
-						<th>Quantity</th>
-						<th>Price</th>
-						<th>Quantity</th>
-						<th>Balance</th>
-					</tr>
-				</tbody>
+			<tbody>
+				<tr>
+					<th rowSpan="2">Symbol</th>
+					<th colSpan="2">From 1/1/2013</th>
+					<th colSpan="3">To 1/1/2016</th>
+					<th rowSpan="2">Gain/Loss($)</th>
+				</tr>
+				<tr>
+					<th>Price($)</th>
+					<th>Quantity</th>
+					<th>Price($)</th>
+					<th>Quantity</th>
+					<th>Balance($)</th>
+				</tr>
+				{/*
+					data.map( (obj)=>(
+						<tr>
+							<td> {obj.symbol} </td>
+							<td> {obj.startPrice} </td>
+							<td> {obj.startQuantity} </td>
+							<td> {obj.startBalance} </td>
+							<td> {obj.endPrice} </td>
+							<td> {obj.endQuantity} </td>
+							<td> {obj.endBalance} </td>
+							<td> {roundUp(obj.endBalance-obj.startBalance, 2)} </td>
+						</tr>
+					))
+				*/}
+			</tbody>
 			</table>
 		);
 	}
 }
 
-export default Table;
+export default Summary;
