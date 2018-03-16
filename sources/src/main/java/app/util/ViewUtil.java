ffc56572f3c69e8729db23a1ef3fc19c668774f4
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-16 00:19:02 -0700

    imports organized, empty lines added

--- sources/src/main/java/app/util/ViewUtil.java
+++ sources/src/main/java/app/util/ViewUtil.java
@@ -2,11 +2,16 @@ package app.util;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.velocity.app.*;
-import org.eclipse.jetty.http.*;
-import spark.*;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.eclipse.jetty.http.HttpStatus;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 import spark.template.velocity.VelocityTemplateEngine;
+
 public class ViewUtil {
 
 	/**
@@ -18,14 +23,17 @@ public class ViewUtil {
 		model.put("WebPath", Path.Web.class);  // Access application URLs from templates
 		return strictVelocityEngine().render( new ModelAndView(model, templatePath));
 	}
+	
 	public static Route notAcceptable = (Request request, Response response) -> {
         response.status(HttpStatus.NOT_ACCEPTABLE_406);
         return "No suitable content found. Please specify either 'html/text' or 'application/json'.";
     };
+    
 	public static Route notFound = (Request request, Response response) -> {
 		response.status(HttpStatus.NOT_FOUND_404);
 		return render(request, new HashMap<>(), Path.Templates.NOT_FOUND); 
 	};
+	
 	private static VelocityTemplateEngine strictVelocityEngine() {
         VelocityEngine configuredEngine = new VelocityEngine();
         configuredEngine.setProperty("runtime.references.strict", true);
