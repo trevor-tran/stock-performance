3522e2173afbe3cdb9a1948eaeb6ba8b72d42db3
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-18 17:05:58 -0700

    session attributes and queryParams rellocated in RequestUtil instead; removed unnecessary

--- sources/src/main/java/app/util/RequestUtil.java
+++ sources/src/main/java/app/util/RequestUtil.java
@@ -4,14 +4,6 @@ import spark.*;
 
 public class RequestUtil {
 
-    public static String getQueryLocale(Request request) {
-        return request.queryParams("locale");
-    }
-
-    public static String getParamIsbn(Request request) {
-        return request.params("isbn");
-    }
-
     public static String getQueryUsername(Request request) {
         return request.queryParams("username");
     }
@@ -19,29 +11,17 @@ public class RequestUtil {
     public static String getQueryPassword(Request request) {
         return request.queryParams("password");
     }
-
-    public static String getQueryLoginRedirect(Request request) {
-        return request.queryParams("loginRedirect");
+    
+    public static String getQueryFirstName(Request request) {
+    	return request.session().attribute("firstName");
     }
 
-    public static String getSessionLocale(Request request) {
-        return request.session().attribute("locale");
-    }
-
-    public static String getSessionCurrentUser(Request request) {
-        return request.session().attribute("currentUser");
-    }
-
-    public static boolean removeSessionAttrLoggedOut(Request request) {
-        Object loggedOut = request.session().attribute("loggedOut");
-        request.session().removeAttribute("loggedOut");
-        return loggedOut != null;
-    }
-
-    public static String removeSessionAttrLoginRedirect(Request request) {
-        String loginRedirect = request.session().attribute("loginRedirect");
-        request.session().removeAttribute("loginRedirect");
-        return loginRedirect;
+    public static String getSessionUserId(Request request) {
+        if(request.session().attribute("currentUserId") != null){
+        	int userId = request.session().attribute("currentUserId");
+        	return String.valueOf(userId);
+        }
+        return null;
     }
 
     public static boolean clientAcceptsHtml(Request request) {
