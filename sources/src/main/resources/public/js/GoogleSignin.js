0717145ced3047b3c7483dcfa2032d8bfff6e10e
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-20 21:00:52 -0700

    onSignOut() to sign out google account. functions about cookies removed

--- sources/src/main/resources/public/js/GoogleSignin.js
+++ sources/src/main/resources/public/js/GoogleSignin.js
@@ -1,52 +1,26 @@
-//https://stackoverflow.com/a/24103596
-function setCookie(name,value,days) {
-	var expires = "";
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + (days*24*60*60*1000));
-		expires = "; expires=" + date.toUTCString();
-	}
-	document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var cookies = document.cookie.split(';');
-	for(var i=0;i < cookies.length;i++) {
-		var c = cookies[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) {
-			return c.substring(nameEQ.length,c.length);
-		}
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	expires = "; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-	document.cookie = name + "=" + "" + expires + "; path=/";
-}
-
 // https://developers.google.com/identity/sign-in/web/
-function onSignIn() 
-{
+function onSignIn() {
 	var googleAuth = gapi.auth2.getAuthInstance();
-
-	if ( googleAuth.isSignedIn.get() )
-	{
+	if ( googleAuth.isSignedIn.get()) {
 		var googleUser = googleAuth.currentUser.get();
 		// The ID token you need to pass to your backend:
 		var idToken = googleUser.getAuthResponse().id_token;
-		// Pass user data to the server w/ a cookie: http://www.javascripter.net/faq/passingp.htm
-		setCookie('currentToken', idToken, 1);
 		window.location.replace("http://" + window.location.host + "/home/");
 		
 		//https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api#example_post_requests
-		/*fetch("http://" + window.location.host + '/signin/',{
+		fetch("http://" + window.location.host + '/googlesignin/', {
 			method: 'POST',
+			credentials:'include',
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 			body:'idtoken='+idToken
-		});*/
+		});
+	}
+}
+
+function onSignOut() {
+	var googleAuth = gapi.auth2.getAuthInstance();
+	if ( googleAuth.isSignedIn.get()){
+		googleAuth.signOut();
 	}
 }
 
