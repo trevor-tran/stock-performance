2aaaf3ae57c75daf0224d821772532e197363981
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-23 16:15:28 -0700

    renamed, first attempt on get data from Quandl, inserted into database, and queried

--- sources/src/main/java/app/stock/StockDao.java
+++ sources/src/main/java/app/stock/StockDao.java
@@ -3,10 +3,13 @@ package app.stock;
 import java.lang.invoke.MethodHandles;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
-import java.util.TreeMap;
 
+import org.apache.commons.dbutils.DbUtils;
 import org.apache.http.HttpException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
@@ -18,22 +21,116 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-/**
- * Responsible for request stock data from quandl.com
- * @author PhuongTran
- *
- */
+
+import app.util.DatabaseConnection;
+
 public class StockDao {
+
 	//quandl api key
 	private static final String apiKey = "LSHfJJyvzYHUyU9jHpn6";
-	private static Map<String,String> summary;
+	public static final int NOT_FOUND = -1;
 	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	public static ResultSet queryStockData(String symbol, String startDate, String endDate){
+		try{
+			Connection connection = DatabaseConnection.getConnection();
+			Statement statement = connection.createStatement();
+			if(findSymbol(statement, symbol) == NOT_FOUND){
+				update(statement, symbol, startDate, endDate);
+			}
+			String sql = String.format( 
+								"SELECT t.price_date, s.symbol, t.price, t.split_ratio "
+							+	"FROM %s AS t "
+							+	"INNER JOIN Symbols AS s "
+							+	"WHERE t.symbol_id = s.symbol_id "
+							+ 	"AND t.price_date BETWEEN '%s' AND '%s'", symbol, startDate, endDate);
+			ResultSet rs = statement.executeQuery(sql);
+			
+			//close(connection, statement);
+			return rs;
+		}catch(SQLException ex){
+			logger.error(ex.getMessage());
+		}catch(Exception ex){
+		}
+		return null;
+	}
 	
+	private static void update(Statement statement, String symbol, String startDate, String endDate){
+		try{
+			JsonArray quandlData = getQuandlData(symbol, startDate, endDate);
+			if(quandlData != null){
+				int symbolId = findSymbol(statement, symbol);
+				if(symbolId == NOT_FOUND){
+					symbolId = addSymbolAndCreateTable(statement, symbol);
+				}
+				for (JsonElement element : quandlData) {
+					//each element is ["2016-12-28", "MSFT", 62.99, 2.0]
+					JsonArray e = element.getAsJsonArray();
+					String date = e.get(0).getAsString(); 
+					double price = e.get(2).getAsDouble();
+					double split = e.get(3).getAsDouble();
+					String sql = String.format("INSERT INTO %s( price_date, price, split_ratio, symbol_id) "
+											+ "VALUES( '%s', %f, %f,%d)", symbol,date,price,split,symbolId);
+					statement.executeUpdate(sql);
+				}
+			}
+		}catch(Exception ex){
+			logger.error(ex.getStackTrace().toString());
+		}
+	}	
+	
+	private static int addSymbolAndCreateTable(Statement statement,String symbol){
+		try{
+			//add new symbol to Symbols table
+			String sql = String.format("INSERT INTO Symbols(symbol) VALUES('%s')", symbol);
+			statement.executeUpdate(sql);
+			
+			//create new table for new symbol added
+			sql = String.format(
+					"CREATE TABLE %s ("
+							+"ticker_id int auto_increment not null,"
+							+"symbol_id int not null,"
+							+"price_date date not null,"
+							+"price decimal(13,2) not null,"
+							+"split_ratio double not null,"
+							+"primary key (ticker_id),"
+							+"foreign key (symbol_id) references Symbols(symbol_id))", symbol);
+			statement.executeUpdate(sql);
+			
+			//return symbol_id from Symbols table
+			return findSymbol(statement,symbol);
+		
+		}catch(SQLException ex){
+			logger.error(ex.getStackTrace().toString());
+		}catch(Exception ex){
+			logger.error(ex.getStackTrace().toString());
+		}
+		return NOT_FOUND;
+	}
+	
+	private static int findSymbol(Statement statement, String symbol) {
+		try{
+			String sql = String.format( "SELECT symbol_id FROM Symbols WHERE symbol='%s'",symbol);
+			ResultSet rs = statement.executeQuery(sql);
+
+			if(rs.next()){
+				int symbolId = rs.getInt("symbol_id");
+				rs.close();
+				return symbolId;
+			}
+		}catch(SQLException ex){
+			logger.error(ex.getStackTrace().toString());
+		}catch(Exception ex){
+			logger.error(ex.getStackTrace().toString());
+		}
+		return NOT_FOUND;
+	}
 
 	//https://hc.apache.org/httpcomponents-client-ga/tutorial/html/fundamentals.html#d5e49
 	//https://github.com/google/gson/blob/master/UserGuide.md
-	public static Map<String,Map<String,Double>> getStockData(long invest, String symbol, String startDate, String endDate) {
+	private static JsonArray getQuandlData(String symbol, String startDate, String endDate) {
 		try{
 			URI uri = getRequestUri(symbol, startDate, endDate);
 			CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -53,7 +150,7 @@ public class StockDao {
 				if(dataArr.size() == 0){
 					return null;
 				}
-				return reformartAndComputeReturn(dataArr,invest);//TODO: experiencing data loss with large amount.
+				return dataArr;
 			} 
 		}catch (HttpException ex){
 			logger.error(ex.getMessage());
@@ -81,57 +178,43 @@ public class StockDao {
 		return uri;
 	}
 
-	private static Map<String,Map<String,Double>> reformartAndComputeReturn(JsonArray dataArr,long investment){
-		summary = new HashMap<String,String>();
-
-		//MUST use TreeMap here to sort dates
-		Map<String,Map<String,Double>> balanceMap = new TreeMap<String,Map<String,Double>>();
-		//compute number of shares (investment divided by price on starting date)  
-		double numberOfShares = investment / dataArr.get(0).getAsJsonArray().get(2).getAsDouble();
-		//numberOfShares = round(numberOfShares,6);
-		for ( int i=0 ; i<dataArr.size() ; i++) {
-			//each element is ["2016-12-28","MSFT",62.99,2.0]
-			JsonArray e = dataArr.get(i).getAsJsonArray();
-			String date = e.get(0).getAsString();
-			String ticker = e.get(1).getAsString(); 
-			double price = e.get(2).getAsDouble();
-			double split = e.get(3).getAsDouble();			
-			if(split != 1d){
-				numberOfShares = numberOfShares * split;
-			}
-			double balance = round(numberOfShares * price, 2);
-			if (balanceMap.containsKey(date)){
-				Map<String,Double> value = balanceMap.get(date);
-				value.put(ticker, balance);
-			}else {
-				Map<String,Double> newValue = new HashMap<String,Double>();
-				newValue.put(ticker, balance);
-				balanceMap.put(date, newValue);
-			}
-			//summary
-			if(i==0){
-				summary.put("symbol", ticker);
-				summary.put("startDate", date);
-				summary.put("startPrice", Double.toString(price));
-				summary.put("startQuantity",Double.toString(numberOfShares));
-				summary.put("startBalance", Double.toString(investment));
-			}else if(i==dataArr.size()-1){
-				summary.put("endDate", date);
-				summary.put("endPrice", Double.toString(price));
-				summary.put("endQuantity",Double.toString(numberOfShares));
-				summary.put("endBalance",Double.toString(balance));
-			}
+	/**
+	 * close <i>Connection,Statement, ResultSet</i>
+	 * @param connection
+	 * @param statement
+	 * @param rs
+	 * @throws SQLException
+	 */
+	private static void close(Connection connection, Statement statement, ResultSet rs){
+		try{
+			if(rs != null){rs.close();}
+			if( statement != null){ statement.close();}
+			if (connection != null){connection.close();}
+		}catch(SQLException ex){
+			logger.error("Database exception:", ex);
+		}finally{
+			DbUtils.closeQuietly(connection);
+			DbUtils.closeQuietly(statement);
+			DbUtils.closeQuietly(rs);
 		}
-		
-		return balanceMap;
-	}
-	public static Map<String,String> getSummary(){
-		return summary;
 	}
 
-	// round numbers to nth decimal places
-	private static double round(double number,int n){
-		double decimal = Math.pow(10, n);
-		return Math.round(number * decimal) / decimal; 
+	/**
+	 * close <i>Connection, Statement</i>
+	 * @param connection
+	 * @param statement
+	 * @throws SQLException
+	 */
+	private static void close(Connection connection, Statement statement){
+		try{
+			if( statement != null){ statement.close();}
+			if (connection != null){connection.close();}
+		}catch(SQLException ex){
+			logger.error("Database exception:", ex);
+		}finally{
+			DbUtils.closeQuietly(connection);
+			DbUtils.closeQuietly(statement);
+		}
 	}
+
 }
