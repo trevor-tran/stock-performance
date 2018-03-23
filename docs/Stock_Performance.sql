6c284fd5080d18239d7c87c90624d44670726f6e
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-22 23:09:34 -0700

    code moved arround

--- docs/Stock_Performance.sql
+++ docs/Stock_Performance.sql
@@ -1,28 +1,5 @@
 use stock_performance;
 
-
--- number of tables(AAPL, MSFT...) increases when more symbols added to Symbols_table                         
-create table Symbols( symbol_id int auto_increment not null,
-						symbol varchar(10) not null,
-                        primary key (symbol));
-insert into symbols(symbol) values('AAPL');
-create table AAPL( ticker_id int auto_increment not null,
-						symbol_id int not null,
-						price_date date not null,
-                        price decimal(13,2) not null,
-                        split_ratio double not null,
-                        primary key (ticker_id),
-                        foreign key (symbol_id) references Symbols(symbol_id));
-insert into symbols(symbol) values('MSFT');
-create table MSFT( ticker_id int auto_increment not null,
-						symbol_id int not null,
-						price_date date not null,
-                        price decimal(13,2) not null,
-                        split_ratio double not null,
-                        primary key (ticker_id),
-                        foreign key (symbol_id) references Symbols(symbol_id));
-
-
 create table UserInfo( user_id int auto_increment,
 					first_name varchar(20) not null,
                     last_name varchar(20) not null,
@@ -45,6 +22,25 @@ create table UserInvestment( investment_id int auto_increment,
                     
 insert into UserInfo(first_name,last_name,username,salt,hashed_password,google_user) values ('phuong','tran','phuong', '$2a$10$YawZPDb7OLQ66FQuMCyW0e','$2a$10$YawZPDb7OLQ66FQuMCyW0e0d2r2Qd1kFLgHLhJqJwaypsQdnYX7fi',0);
 
+-- number of tables(AAPL, MSFT...) increases when more symbols added to Symbols_table                         
+create table Symbols( symbol_id int auto_increment not null,
+						symbol varchar(10) not null,
+                        primary key (symbol_id));
 
 
-                            
+
+create table AAPL( ticker_id int auto_increment not null,
+					price_date date not null,
+                    price decimal(13,2) not null,
+                    split_ratio double not null,
+					symbol_id int not null,
+                    primary key (ticker_id),
+                    foreign key (symbol_id) references Symbols(symbol_id));
+create table MSFT( ticker_id int auto_increment not null,
+					price_date date not null,
+                    price decimal(13,2) not null,
+                    split_ratio double not null,
+                    symbol_id int not null,
+                    primary key (ticker_id),
+                    foreign key (symbol_id) references Symbols(symbol_id));
+
