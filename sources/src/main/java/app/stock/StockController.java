5035055759fb49994814207652f014fe7de5baf3
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-23 17:50:00 -0700

    removed methods that are in StockDao class

--- sources/src/main/java/app/stock/StockController.java
+++ sources/src/main/java/app/stock/StockController.java
@@ -1,26 +1,13 @@
 package app.stock;
 
 import java.lang.invoke.MethodHandles;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.apache.http.HttpException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.mysql.jdbc.BalanceStrategy;
 
 import app.util.QueryHandler;
 /**
@@ -29,8 +16,7 @@ import app.util.QueryHandler;
  *
  */
 public class StockController {
-	//quandl api key
-	private static final String apiKey = "LSHfJJyvzYHUyU9jHpn6";
+
 	private static Map<String,String> summary;
 	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
@@ -92,68 +78,7 @@ public class StockController {
 		return null; //TODO: need a proper return
 
 	}
-
-	private static Map<String,Map<String,Double>> reformartAndComputeReturn(JsonArray dataArr,long investment){
-		summary = new HashMap<String,String>();
-
-		//MUST use TreeMap here to sort dates
-		Map<String,Map<String,Double>> balanceMap = new TreeMap<String,Map<String,Double>>();
-		//compute number of shares (investment divided by price on starting date)  
-		double numberOfShares = investment / dataArr.get(0).getAsJsonArray().get(2).getAsDouble();
-		//numberOfShares = round(numberOfShares,6);
-		for ( int i=0 ; i<dataArr.size() ; i++) {
-			//each element is ["2016-12-28","MSFT",62.99,2.0]
-			JsonArray e = dataArr.get(i).getAsJsonArray();
-			String date = e.get(0).getAsString();
-			String ticker = e.get(1).getAsString(); 
-			double price = e.get(2).getAsDouble();
-			double split = e.get(3).getAsDouble();			
-			if(split != 1d){
-				numberOfShares = numberOfShares * split;
-			}
-			double balance = round(numberOfShares * price, 2);
-			if (balanceMap.containsKey(date)){
-				Map<String,Double> value = balanceMap.get(date);
-				value.put(ticker, balance);
-			}else {
-				Map<String,Double> newValue = new HashMap<String,Double>();
-				newValue.put(ticker, balance);
-				balanceMap.put(date, newValue);
-			}
-			//summary
-			if(i==0){
-				summary.put("symbol", ticker);
-				summary.put("startDate", date);
-				summary.put("startPrice", Double.toString(price));
-				summary.put("startQuantity",Double.toString(numberOfShares));
-				summary.put("startBalance", Double.toString(investment));
-			}else if(i==dataArr.size()-1){
-				summary.put("endDate", date);
-				summary.put("endPrice", Double.toString(price));
-				summary.put("endQuantity",Double.toString(numberOfShares));
-				summary.put("endBalance",Double.toString(balance));
-			}
-		}
-
-		return balanceMap;
-	}
-
-	/*	//build URL to request data from Quandl
-	private static URI getRequestUri(String symbol, String startDate, String endDate) throws URISyntaxException{
-		//https://docs.quandl.com/docs/parameters-1
-		URI uri = new URIBuilder()
-				.setScheme("https")
-				.setHost("quandl.com")
-				.setPath("/api/v3/datatables/WIKI/PRICES.json")
-				.setParameter("qopts.columns", "date,ticker,close,split_ratio")
-				.setParameter("date.gte", startDate)
-				.setParameter("date.lte", endDate)
-				.setParameter("ticker", symbol)
-				.setParameter("api_key", apiKey)
-				.build();
-		return uri;
-	}*/
-
+	
 	public static Map<String,String> getSummary(){
 		return summary;
 	}
