fd3cb1e03d553623e53cf52719b9cb2b839d2303
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-27 15:53:39 -0700

    singleton class to create and close db connections

--- sources/src/main/java/app/util/ConnectionManager.java
+++ sources/src/main/java/app/util/ConnectionManager.java
@@ -1,105 +1,53 @@
 package app.util;
 
 import java.lang.invoke.MethodHandles;
-import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.apache.commons.dbutils.DbUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-public class ConnectionManager implements AutoCloseable {
-		private CallableStatement cstmt;
-		private PreparedStatement pstmt;
-		private Statement stmt;
-		private Connection connection;
-		
+
+public class ConnectionManager {
+
+	private static ConnectionManager instance = null;
 	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	public ConnectionManager(){
-		try{
-			connection = DatabaseConnection.getConnection();
-		}catch(SQLException ex){
-			logger.error(ex.getMessage());
-		}catch(Exception ex){
-			logger.error(ex.getMessage());
-		}
-	}
-	
-	public PreparedStatement prepareStatement(String sql){
-		try{
-			pstmt = connection.prepareStatement(sql);
-			return pstmt;
-		}catch(SQLException ex){
-			logger.error(ex.getMessage());
-		}catch(Exception ex){
-			logger.error(ex.getMessage());
-		}
-		return null;
-	}
-	
-	public CallableStatement prepareCall(String sql){
-		try{
-			cstmt = connection.prepareCall(sql);
-			return cstmt;
-		}catch(SQLException ex){
-			logger.error(ex.getMessage());
-		}catch(Exception ex){
-			logger.error(ex.getMessage());
-		}
-		return null;
-	}
-	
-	public ResultSet executeQueryViaStatement(String sql){
-		try{
-			stmt = connection.createStatement();
-			return stmt.executeQuery(sql);
-		}catch(SQLException ex){
-			logger.error(ex.getMessage());
-		}catch(Exception ex){
-			logger.error(ex.getMessage());
-		}
-		return null;
-	}
-	
-	public void executeUpdateViaStatement(String sql){
-		try{
-			stmt = connection.createStatement();
-			 stmt.executeUpdate(sql);
-		}catch(SQLException ex){
-			logger.error(ex.getMessage());
-		}catch(Exception ex){
-			logger.error(ex.getMessage());
-		}
+	private ConnectionManager(){		
 	}
 
-	@Override
-	public void close(){
+	public static ConnectionManager getInstance() {
+		if(instance == null){
+			synchronized (ConnectionManager.class) {
+				if(instance == null)
+					instance = new ConnectionManager();
+			}
+		}
+		return instance;
+	}
+
+	public Connection getConnection(){
 		try{
-			if( stmt != null){ 
-				stmt.close();
-			}
-			if( pstmt != null){ 
-				pstmt.close();
-			}
-			if( cstmt != null){ 
-				cstmt.close();
-			}
-			if (connection != null){
-				connection.close();
+			return  DatabaseConnection.getConnection();
+		}catch(SQLException ex){
+			logger.error(ex.getMessage());
+		}catch(Exception ex){
+			logger.error(ex.getMessage());
+		}
+		return null;
+	}
+
+	public void releaseConnection(Connection conn){
+		try{
+			if (conn != null){
+				conn.close();
 			}
 		}catch(SQLException ex){
 			logger.error("Database exception:", ex);
 		}catch(Exception ex){
 			logger.error(ex.getMessage());
 		}finally{
-			DbUtils.closeQuietly(connection);
-			DbUtils.closeQuietly(stmt);
-			DbUtils.closeQuietly(pstmt);
-			DbUtils.closeQuietly(cstmt);
+			DbUtils.closeQuietly(conn);
 		}
 	}
 
