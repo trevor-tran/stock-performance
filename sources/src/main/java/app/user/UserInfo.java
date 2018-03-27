a0e32e788782ca709e8253af589cb74b078e4d5d
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-26 22:03:20 -0700

    using Set to store stock symbols

--- sources/src/main/java/app/user/UserInfo.java
+++ sources/src/main/java/app/user/UserInfo.java
@@ -3,11 +3,12 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Set;
 @Value // All fields are private and final. Getters (but not setters) are generated (https://projectlombok.org/features/Value.html)
 public class UserInfo {
 	BigDecimal investment;
 	Date startDate;
 	Date endDate;
-	String stockSymbol;
+	Set<String> stockSymbol;
 	double numberOfShares;
 }
