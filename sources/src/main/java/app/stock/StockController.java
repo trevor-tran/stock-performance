bca57e4c79c5a1602e808d07f912108b8bb3973b
PhuongTran <tranduyphuong2110@gmail.com>
2018-03-31 02:04:48 -0700

    removed commented code

--- sources/src/main/java/app/stock/StockController.java
+++ sources/src/main/java/app/stock/StockController.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class StockController {
 
-	private static Map<String,String> summary;
+	private static Map<String,Map<String,String>> summary;
 	protected static Set<String> symbols = new HashSet<String>();
 	final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
@@ -26,7 +26,7 @@ public class StockController {
 			if(data != null){
 				//https://www.geeksforgeeks.org/iterate-map-java/
 				Iterator< Map.Entry<String,List<Stock>>> iterator = data.entrySet().iterator();
-				
+
 				//get first entry
 				Map.Entry<String,List<Stock>> entry = iterator.next();
 				//e.g {"MSFT":14.2 , "AAPL":10.5 , "GOOGL":10.0}
@@ -34,7 +34,7 @@ public class StockController {
 				// singleDayBalances ==	{symbol:balance} e.g {"MSFT": 5000,"GOOGL": 10000}
 				Map<String,Double> singleDayBalances = computeBalances(entry.getValue(), quantityOfStocks);
 				balances.put(entry.getKey(), singleDayBalances);
-				
+
 				while(iterator.hasNext() ){
 					entry = iterator.next();
 					singleDayBalances = computeBalances(entry.getValue(), quantityOfStocks);
@@ -42,51 +42,6 @@ public class StockController {
 				}
 				return balances;
 			}
-			/*if(rs != null){
-				summary = new HashMap<String,String>();
-				//MUST use TreeMap here to sort dates
-				Map<String,Map<String,Double>> balanceMap = new TreeMap<String,Map<String,Double>>();
-				double numberOfShares=0;
-				//["2016-12-28","MSFT",62.99,2.0]
-				while(rs.next()){
-					if(rs.isFirst()){
-						//compute number of shares (investment divided by price on starting date)  
-						numberOfShares = investment / rs.getDouble("price");
-					}
-
-					String date = rs.getString("date_as_id");
-					String ticker = rs.getString("symbol"); 
-					double price = rs.getDouble("price");
-					double split = rs.getDouble("split_ratio");			
-					if(split != 1d){
-						numberOfShares = numberOfShares * split;
-					}
-					double balance = round(numberOfShares * price, 2);
-					if (balanceMap.containsKey(date)){
-						Map<String,Double> value = balanceMap.get(date);
-						value.put(ticker, balance);
-					}else {
-						Map<String,Double> newValue = new HashMap<String,Double>();
-						newValue.put(ticker, balance);
-						balanceMap.put(date, newValue);
-					}
-					//summary
-					if(rs.isFirst()){
-						summary.put("symbol", ticker);
-						summary.put("startDate", date);
-						summary.put("startPrice", Double.toString(price));
-						summary.put("startQuantity",Double.toString(numberOfShares));
-						summary.put("startBalance", Double.toString(investment));
-					}else if(rs.isLast()){
-						summary.put("endDate", date);
-						summary.put("endPrice", Double.toString(price));
-						summary.put("endQuantity",Double.toString(numberOfShares));
-						summary.put("endBalance",Double.toString(balance));
-					}
-				}
-				rs.close();
-				return balanceMap;		
-				}*/
 		}catch(Exception ex){
 			logger.error("getData() failed." + ex.getStackTrace());	
 		}
@@ -117,9 +72,9 @@ public class StockController {
 		}
 		return quantityOfStocks;
 	}
-
+	//TODO: temporarily return null to avoiding fixing errors for now.
 	public static Map<String,String> getSummary(){
-		return summary;
+		return null;
 	}
 
 	// round numbers to nth decimal places
