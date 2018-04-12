ff7ad3f755e70bb48f94a13bc3b39f1c97f8c489
PhuongTran <tranduyphuong2110@gmail.com>
2018-04-11 20:42:00 -0700

    renamed variables

--- sources/src/main/java/com/phuongdtran/home/HomeController.java
+++ sources/src/main/java/com/phuongdtran/home/HomeController.java
@@ -25,10 +25,10 @@ public class HomeController {
 			}
 			else if (clientAcceptsJson(request)) {
 				long budget = Long.parseLong(request.queryParams("budget"));
-				String start = request.queryParams("start");
-				String end = request.queryParams("end");
+				String startDate = request.queryParams("start");
+				String endDate = request.queryParams("end");
 				String symbol = request.queryParams("symbol");
-				Map<String,Map<String,Double>> data = StockController.getData(budget, symbol, start, end);
+				Map<String,Map<String,Double>> data = StockController.getData(budget, symbol, startDate, endDate);
 				//TODO: handle null data
 				response.header("Content-Type", "application/json");
 				return dataToJson(data);
