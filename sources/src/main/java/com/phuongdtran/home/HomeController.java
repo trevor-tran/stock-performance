9c55bb4e91813d77dc95de780ce504729f0a0d81
PhuongTran <tranduyphuong2110@gmail.com>
2018-04-12 21:14:21 -0700

    update Investment when front-end sends request data to backend

--- sources/src/main/java/com/phuongdtran/home/HomeController.java
+++ sources/src/main/java/com/phuongdtran/home/HomeController.java
@@ -1,4 +1,6 @@
 package com.phuongdtran.home;
+
+import static com.phuongdtran.util.RequestUtil.*;
 import static com.phuongdtran.stock.StockController.getSummary;
 import static com.phuongdtran.util.JsonUtil.dataToJson;
 import static com.phuongdtran.util.RequestUtil.clientAcceptsHtml;
@@ -7,6 +9,8 @@ import static com.phuongdtran.util.RequestUtil.clientAcceptsJson;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.phuongdtran.investment.Investment;
+import com.phuongdtran.investment.InvestmentController;
 import com.phuongdtran.signin.SigninController;
 import com.phuongdtran.stock.StockController;
 import com.phuongdtran.util.Path;
@@ -24,10 +28,15 @@ public class HomeController {
 				return ViewUtil.render(request, model, Path.Templates.HOME);
 			}
 			else if (clientAcceptsJson(request)) {
+				
 				long budget = Long.parseLong(request.queryParams("budget"));
 				String startDate = request.queryParams("start");
 				String endDate = request.queryParams("end");
 				String symbol = request.queryParams("symbol");
+				Investment inv = getSessionInvestment(request);
+				int userId = Integer.parseInt(getSessionUserId(request));
+				
+				InvestmentController.updateInvestment(userId, inv, startDate, endDate, symbol, budget);
 				Map<String,Map<String,Double>> data = StockController.getData(budget, symbol, startDate, endDate);
 				//TODO: handle null data
 				response.header("Content-Type", "application/json");
