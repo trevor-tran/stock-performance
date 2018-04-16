2297723cb8de6fd08f253ad9e0cf5a2abf03dae0
PhuongTran <tranduyphuong2110@gmail.com>
2018-04-15 21:15:21 -0700

    jsonArray to Set, a separate Route to fetch stock data

--- sources/src/main/java/com/phuongdtran/home/HomeController.java
+++ sources/src/main/java/com/phuongdtran/home/HomeController.java
@@ -7,10 +7,13 @@ import static com.phuongdtran.util.RequestUtil.clientAcceptsHtml;
 import static com.phuongdtran.util.RequestUtil.clientAcceptsJson;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.phuongdtran.investment.Investment;
 import com.phuongdtran.investment.InvestmentController;
@@ -24,25 +27,30 @@ import spark.Response;
 import spark.Route;
 public class HomeController {
 
-	public static Route fetchOneStock = (Request request, Response response) -> {
-		if(SigninController.isSignIn(request, response)){
-			if (clientAcceptsHtml(request)) {
-				HashMap<String, Object> model = new HashMap<>();
-				return ViewUtil.render(request, model, Path.Templates.HOME);
-			}
-			else if (clientAcceptsJson(request)) {
-				Gson gson = new GsonBuilder().create();
-				JsonObject json = gson.fromJson(request.body(), JsonObject.class);
-				InvestmentController.updateInvestment(request);
-				Investment inv = getSessionInvestment(request);
-				Map<String,Map<String,Double>> data = StockController.getData(inv);
-				//TODO: handle null data
-				response.header("Content-Type", "application/json");
-				return dataToJson(data);
-			}
-			return ViewUtil.notAcceptable.handle(request, response);
+	public static Route handleHomeDisplay = (Request request, Response response) -> {
+		if(SigninController.isSignIn(request, response) && clientAcceptsHtml(request)){
+			HashMap<String, Object> model = new HashMap<>();
+			return ViewUtil.render(request, model, Path.Templates.HOME);
 		}
-		return null;
+		return ViewUtil.notAcceptable.handle(request, response);
+	};
+
+	public static Route fetchData = (Request request, Response response) -> {
+		if (SigninController.isSignIn(request, response) && clientAcceptsJson(request)) {
+			Gson gson = new GsonBuilder().create();
+			JsonObject json = gson.fromJson(request.body(), JsonObject.class);
+			String budget = json.get("budget").getAsString();
+			String startDate = json.get("startdate").getAsString();
+			String endDate = json.get("enddate").getAsString();
+			Set<String> symbols = jsonArrayToSet( json.get("symbols").getAsJsonArray());
+			InvestmentController.updateInvestment(request);
+			Investment inv = getSessionInvestment(request);
+			Map<String,Map<String,Double>> data = StockController.getData(inv);
+			//TODO: handle null data
+			response.header("Content-Type", "application/json");
+			return dataToJson(data);
+		}
+		return ViewUtil.notAcceptable.handle(request, response);
 	};
 
 	public static Route fetchSummary = (Request request, Response response) -> {
@@ -53,4 +61,12 @@ public class HomeController {
 		}
 		return ViewUtil.notAcceptable.handle(request, response);
 	};
+
+	private static Set<String> jsonArrayToSet(JsonArray jsonArray){
+		Set<String> symbols = new HashSet<String>();
+		for(int i=0; i<jsonArray.size(); i++){
+			symbols.add(jsonArray.get(i).getAsString());
+		}
+		return symbols;
+	}
 }
