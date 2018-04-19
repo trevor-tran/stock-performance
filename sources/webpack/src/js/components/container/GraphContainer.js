b5058a8d62bc76ce280a1830e42c8452d233fcd2
PhuongTran <tranduyphuong2110@gmail.com>
2018-04-18 20:43:05 -0700

    unnessary functions removed, using only one fecthData() function

--- sources/webpack/src/js/components/container/GraphContainer.js
+++ sources/webpack/src/js/components/container/GraphContainer.js
@@ -19,15 +19,6 @@ function getLastSymbol(symbols){
 	return symbols[symbols.length-1];
 }
 
-//return e.g.  http://localhost:4567/home/?budget=1&start=1993-1-1&end=1994-1-2&symbol=AAPL
-function buildUrl (budget, start, end, symbol) {
-	var param="?budget=" + budget + "&start=" + start + "&end=" + end;
-	if (symbol != "" && symbol!= null){
-		param += '&symbol='+ symbol;
-	}
-	return '/home/' + param;
-}
-
 //merge current and new data. Return merged data
 function mergeData( currentData, newData){
 	//if newData contains multiple symbols data, return newData, no merging
@@ -73,76 +64,21 @@ function manipulateData(json) {
 	return data;
 }
 
-//get data from server and return formated data
-function fetchData(budget, startDate, endDate, ticker) {
-	return new Promise(function(resolve, reject) {
-		const url = buildUrl(budget, startDate, endDate, ticker);
-		console.log(url);
-		//https://developers.google.com/web/updates/2015/03/introduction-to-fetch#sending_credentials_with_a_fetch_request
-		fetch(url,{
-			credentials: 'include',//crucial to have this to send session attributes, cookies,.... 
-			headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'  } 
-		})
-		.then(function(response) {
-			// convert to JSON
-			return response.json();
-		})
-		.then(function(json) {
-			if(!json){
-				reject(new Error("not_found"));
-			}else{
-				//send notification data updated
-				PubSub.publish("data_updated");
-				resolve( manipulateData(json));
-			}
-		});
-	});
-}
-
 //request and return new data from server for every stock in the stock symbols list
 //nextInput is equivalent to nextProps.getState 
-function fetchAllStocks(currentState){
-	/*return new Promise(function(resolve, reject){
-		var symbols = nextInput.symbols; 
-		var fetchTasks= [];
-		var data; 
-		symbols.forEach(function(symbol){
-			//a recent symbol added at the end of "symbols" list
-			// must fetch the recent added symbol first to update mutual IPO and Delisting date at server side
-			fetchTasks.unshift( function(callback) {
-				fetchData( nextInput.budget, nextInput.start, nextInput.end, symbol)
-				.then(function(newData){
-					callback(null,newData);
-				});
-			});
-		})
-		async.parallel(fetchTasks, function (err,results) { 
-			results.forEach( function(result) {
-				if(data){
-					data = mergeData(data,result);
-				}else {
-					data = result;
-				}
-			});
-			resolve(data);
-		});
-	}, function(err){
-		console.log(err);
-	});*/
-	
+function fetchData(budget, startDate, endDate, symbolList){
 	return new Promise(function(resolve, reject) {
-		//const url = buildUrl(budget, startDate, endDate, ticker);
-		//console.log(url);
-		var budget = currentState.budget;
-		var startDate = currentState.start;
-		var endDate = currentState.end;
-		var symbols = currentState.symbols;
+		if(!Array.isArray(symbolList)){
+			list = [];
+			list.push(symbolList);
+			symbolList = list;
+		}
 		//https://developers.google.com/web/updates/2015/03/introduction-to-fetch#sending_credentials_with_a_fetch_request
 		fetch("/stockdata/",{
 			method: 'POST',
 			credentials: 'include',//crucial to have this to send session attributes, cookies,.... 
 			headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'  },
-			body: JSON.stringify({"budget": budget, "startdate": startDate, "enddate": endDate, "symbols": symbols})
+			body: JSON.stringify({"budget": budget, "startdate": startDate, "enddate": endDate, "symbols": symbolList})
 		})
 		.then(function(response) {
 			// convert to JSON
@@ -169,19 +105,16 @@ class GraphContainer extends Component{
 	}
 
 	componentWillReceiveProps(nextProps){
-		$(".spinner").show();
 		var _self = this;
 		var current = this.props.getState;
 		var next = nextProps.getState;
 		if((current.start!== next.start) || (current.end !== next.end) || (current.budget != next.budget)) {
 			//at least one symbol in the list to fetch
 			if(next.symbols.length){
-				fetchAllStocks(next).then( function(newData){
+				fetchData(next.budget, next.startDate, next.endDate, next.symbols).then( function(newData){
 					setStateAndSave(_self,newData);
-					$(".spinner").hide();
 				});
 			}else{
-				$(".spinner").hide();
 				alert("Should have at least one symbol to display.")
 			}
 		//must compare length to avoid running into this "if" when a symbol removed
@@ -190,9 +123,7 @@ class GraphContainer extends Component{
 			.then( function(newData) {
 				var mergedData = mergeData(_self.state.data , newData);
 				setStateAndSave(_self,mergedData);
-				$(".spinner").hide();
 			}).catch(function(err){
-				$(".spinner").hide();
 				if(err.message==="not_found"){
 					var unfound = getLastSymbol(next.symbols);
 					_self.props.deleteSymbol(getLastSymbol(next.symbols));
@@ -210,14 +141,18 @@ class GraphContainer extends Component{
 				delete obj[deletedSymbol];
 			});
 			setStateAndSave(_self, clonedData);
-			$(".spinner").hide();
 		}
 	}
 
 	componentWillMount() {
 		var _self = this;
 		if(this.props.getState.symbols.length != 0 && this.state.data.length == 0){
-			fetchAllStocks(this.props.getState)
+			var budget = this.props.getState.budget;
+			var startDate = this.props.getState.start;
+			var endDate = this.props.getState.end;
+			var symbolList = this.props.getState.symbols;
+			
+			fetchData(budget, startDate, endDate, symbolList)
 			.then( function(newData) {
 				setStateAndSave(_self, newData);
 			});
