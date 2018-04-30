56a8d8bcc47c49252c0370820083c82d3fd68a83
PhuongTran <tranduyphuong2110@gmail.com>
2018-04-29 22:01:18 -0700

    renamed, fields added

---
+++ sources/src/main/java/com/phuongdtran/stock/SummaryAttribute.java
@@ -0,0 +1,13 @@
+package com.phuongdtran.stock;
+import lombok.*;
+
+public class SummaryAttribute {
+	//@Getter @Setter private String symbol;
+	@Getter @Setter private double startPrice;
+	@Getter @Setter private double startQuantity;
+	@Getter @Setter private double startBalance;
+	@Getter @Setter private double endPrice;
+	@Getter @Setter private double endQuantity;
+	@Getter @Setter private double endBalance;
+
+}
