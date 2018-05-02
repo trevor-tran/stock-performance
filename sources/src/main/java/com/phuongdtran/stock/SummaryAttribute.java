c40724db40cd237ea02021e75cc326f2bb17bada
PhuongTran <tranduyphuong2110@gmail.com>
2018-05-01 22:06:17 -0700

    symbol field added

--- sources/src/main/java/com/phuongdtran/stock/SummaryAttribute.java
+++ sources/src/main/java/com/phuongdtran/stock/SummaryAttribute.java
@@ -2,7 +2,7 @@ package com.phuongdtran.stock;
 import lombok.*;
 
 public class SummaryAttribute {
-	//@Getter @Setter private String symbol;
+	@Getter @Setter private String symbol;
 	@Getter @Setter private double startPrice;
 	@Getter @Setter private double startQuantity;
 	@Getter @Setter private double startBalance;
