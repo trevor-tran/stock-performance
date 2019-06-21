9a231dfecf7f7e325c76696e8a2a819a19586f4c
Phuong Tran <ptran816@uw.edu>
2019-06-20 23:44:55 -0700

    using react hooks to print out values

--- frontend/src/components/SigninForm.js
+++ frontend/src/components/SigninForm.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper, FormControl, Input, Button } from '@material-ui/core';
 
 import './css/Form.css'
 
 function SigninForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <Paper className="signin-form">
         <h1 className="title">Sign In</h1>
         <p>Enter your username and password below</p>
         <FormControl>
           <label>Username</label>
-          <Input className="text-field" placeholder="Enter user name" required />
+          <Input 
+            className="text-field" 
+            placeholder="Enter user name" 
+            required
+            onChange={e => setUsername(e.target.value)}
+          />
           <label>Password</label>
-          <Input className="text-field" type="password" name="password" placeholder="Enter password" required />
+          <Input 
+            className="text-field" 
+            type="password" 
+            placeholder="Enter password" 
+            required
+            onChange={e => setPassword(e.target.value)} 
+          />
           <Button className="submit" variant="contained" color="primary">Submit</Button>
         </FormControl>
     </Paper>
