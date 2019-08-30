ee7b0f44a91c52c04470375b0cdd7e5f5fb97827
Phuong Tran <ptran816@uw.edu>
2019-08-29 19:58:41 -0700

    standard msg format

---
+++ src/main/java/com/phuongdtran/util/Message.java
@@ -0,0 +1,11 @@
+package com.phuongdtran.util;
+
+public class Message {
+	private String status;
+	private String msg;
+	
+	public Message(String status, String msg) {
+		this.status = status;
+		this.msg = msg;
+	}
+}
