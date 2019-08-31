0e023c97e9fc72575e367e2d98432c5cd374fdac
Phuong Tran <ptran816@uw.edu>
2019-08-30 17:04:06 -0700

    restructured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
-
-// other components
-import SigninForm from './components/SigninForm';
-import SignupForm from './components/SignupForm';
-import Header from './components/Header';
-
-//css
 import './App.css';
+import MainPage from './components/MainPage';
+
 
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-      <Header/>
-        <Switch>
-        <Route exact path="/signin/" component={SigninForm} />
-        <Route exact path="/signup/" component={SignupForm} />
-        <Route path="*" render={() => (
-          <Redirect to="/signin/" />
-        )} />
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <MainPage/>
   );
 }
 
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import update from 'immutability-helper';
 import moment from 'moment';
 
+//routers
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+// other components
+import SigninForm from './SigninForm';
+import SignupForm from './SignupForm';
+import Header from './Header';
+
+
 function MainPage() {
   const [budget, setBudget] = useState("");
-  const [start_date, setStartDate ] = useState("");
+  const [start_date, setStartDate] = useState("");
   const [end_date, setEndDate] = useState("");
   const [symbols, setSymbols] = useState([]);
   const [deleted_symbol, setDeletedSymbol] = useState("");
+  return (
+    <React.Fragment>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/signin/" component={SigninForm} />
+          <Route exact path="/signup/" component={SignupForm} />
+          <Route path="*" render={() => (
+            <Redirect to="/signin/" />
+          )} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  )
 }
 
 export default MainPage;
