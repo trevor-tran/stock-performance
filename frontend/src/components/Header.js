5a4dffebffa05457aa7e77c8d1c7403694380100
Phuong Tran <ptran816@uw.edu>
2019-08-31 23:54:04 -0700

    logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Link, Redirect, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React,{useContext} from 'react';
+import { Link } from "react-router-dom";
 import { Button, Typography, Toolbar, AppBar, FormControl } from '@material-ui/core';
-import SigninForm from './SigninForm';
-import SignupForm from './SignupForm';
+import {withRouter} from 'react-router-dom'
+
+import { Context } from '../store';
+
 import "./css/Header.css";
-import { withRouter } from 'react-router-dom'
 
 
-function Header() {
+const Header = withRouter(({history}) => {
+  const {state, dispatch} = useContext(Context)
+  const signout = () => {
+    dispatch({type:"SET_USER", payload: ""})
+    history.push('/signin/')
+  }
 
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6">Stock Performance</Typography>
         <FormControl id="menu">
-          <Button 
-            component={Link} 
-            to='/signin/' 
-            className="button" 
-            color="inherit"
-          > 
-            Sign in
-          </Button>
+          {/* current_user indicates login/logout status. 
+          It stores current username if a user logged in. Otherwise, it's falsy  */}
+          {!state.current_user ? 
+            (<Button component={Link} to='/signin/' className="button" color="inherit"> Sign in </Button>) : 
+            (<Button className="button" color="inherit" onClick={signout}>Sign out</Button>)
+          }
           <Button 
             component={Link} 
             to='/signup/' 
@@ -36,6 +40,6 @@ function Header() {
     </AppBar>
 
   );
-}
+})
 
 export default Header;
