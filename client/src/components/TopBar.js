c126eeacbc99bae7386ed108644535375a799fe6
Phuong Tran <ptran816@uw.edu>
2019-09-14 17:07:04 -0700

    added components

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,11 +1,42 @@
 import React from 'react'
-import { Paper } from '@material-ui/core'
+import { Paper, Button, TextField } from '@material-ui/core'
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
 
-function TopBar () {
+
+function TopBar() {
+
+  const submit = () => {
+    console.log("submited")
+  }
+
   return (
-    <Paper>
-      <p> topbar</p>
-    </Paper>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Paper className="top-bar">
+
+        <TextField id="budget" label="Budget($)" margin="normal" variant="outlined" />
+
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          label="Start date"
+        />
+
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          label="End date"
+        />
+        
+        <TextField id="symbol" label="Symbol:" margin="normal" variant="outlined" />
+
+        <Button variant="outlined" color="primary" onClick={submit}>Update</Button>
+      </Paper>
+    </MuiPickersUtilsProvider>
   )
 }
 
