aa3a70a7cc629a4deed924e61218153a6499af02
Phuong Tran <ptran816@uw.edu>
2020-06-29 23:34:53 -0700

    symbol removing feature worked

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext, useState } from "react";
 import { LineChart, ResponsiveContainer, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-import { data, series} from './utils/data';
+import { data, series } from './utils/data';
 import { Paper } from "@material-ui/core";
 
 import { Context } from '../store';
@@ -27,35 +27,57 @@ function Chart() {
   //   lines.push(<Line type="monotone" key={symbol} dataKey={symbol} dot={false} unit=" USD" stroke={color} />);
   // });
   useEffect(() => {
-          // validate info on server (check if username already exists etc.)
-          if (!Array.isArray(state.symbols) || state.symbols.length == 0) return;
+    // validate info on server (check if username already exists etc.)
 
-          let url = urls.SERVER_URL + urls.DATA;
-          fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-              symbols: state.symbols
-            }),
-            headers:{
-              'Content-Type': 'application/json'
-            }
-          }).then(response => {
-            return response.json();
+    // there are TWO actions changing state.symbols
+    // a symbol is either added to the list or removed from the list
 
-          }).then (json => {
-            if (json.success) {
-              setParsedData(dataParser(state.symbols, JSON.parse(json.msg)));
-            }else {
-              throw json.msg;
-            }
-          }).catch(err => {
-            console.error(err);
-          });
+    if (!Array.isArray(state.symbols) || state.symbols.length == 0) {
+      setParsedData([]);
+    } else if (parsedData.length > state.symbols.length) {
+      // a symbol removed from state.symbols, therefore corresponding stock data in parsedData also need removed
+      // no API calls needed
+      console.log("a symbol removed")
+      const parsedDataAfterSymbolRemoved = [];
+      parsedData.forEach(obj => {
+        if (state.symbols.includes(obj.symbol)) {
+          const copy = {...obj};
+          parsedDataAfterSymbolRemoved.push(copy);
+        }
+      });
+      setParsedData(parsedDataAfterSymbolRemoved);
+    } else {
+      // need to get stock data from server
+      console.log("fetching data from server...");
 
-  },[state.symbols]);
+      let url = urls.SERVER_URL + urls.DATA;
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          symbols: state.symbols
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        return response.json();
 
+      }).then(json => {
+        if (json.success) {
+          setParsedData(dataParser(state.symbols, JSON.parse(json.msg)));
+        } else {
+          throw json.msg;
+        }
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  }, [state.symbols]);
+
+  // data fetched from server is in different format from what expected to use in Rechart
+  // reference utils/data.js to see sample data used in Rechart.
+  // this function parses server-format data to Rechart-format data
   const dataParser = (symbols, json) => {
-    console.log(json)
     let parsedData = [];
     symbols.map(symbol => {
       const data = json[symbol];
@@ -63,7 +85,8 @@ function Chart() {
       stockInfoForASymbol["symbol"] = symbol;
       stockInfoForASymbol["data"] = new Array();
       data.map(oneday => {
-        stockInfoForASymbol["data"].push({date: moment(oneday.date, "YYYY-MM-DD", true).format("DD MMM. YYYY"), value: Number(oneday.price)});
+        const formatedDate = moment(oneday.date, "YYYY-MM-DD", true).format("DD MMM. YYYY");
+        stockInfoForASymbol["data"].push({ date: formatedDate, value: Number(oneday.price) });
       });
       stockInfoForASymbol["data"].sort((o1, o2) => o1.date.localeCompare(o2.date));
       parsedData.push(stockInfoForASymbol);
@@ -72,7 +95,7 @@ function Chart() {
   }
 
   return (
-    <Paper className="graph" style={{width: "80vw", height:"60vh", margin:'auto'}}>
+    <Paper className="graph" style={{ width: "80vw", height: "60vh", margin: 'auto' }}>
       <ResponsiveContainer width='100%' height='100%'>
         {/* <LineChart data={data}>
           <XAxis dataKey="date" angle={-20} textAnchor="end" height={55} />
@@ -88,13 +111,13 @@ function Chart() {
           {/* <XAxis dataKey="name" />
           <YAxis /> */}
           <XAxis dataKey="date" type="category" allowDuplicatedCategory={false} angle={-20} textAnchor="end" height={55} />
-        <YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }}/>
+          <YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }} />
           <Tooltip />
           <Legend />
           <Legend />
-        {parsedData.map(s => (
-          <Line dataKey="value" data={s.data} name={s.symbol} key={s.symbol} />
-        ))}
+          {parsedData.map(s => (
+            <Line dataKey="value" data={s.data} name={s.symbol} key={s.symbol} />
+          ))}
           {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" />
           <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
         </LineChart>
