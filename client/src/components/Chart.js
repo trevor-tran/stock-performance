31f5cde2384d7d9b058494ceead2d7fe0ea22bab
Phuong Tran <ptran816@uw.edu>
2020-07-06 23:50:09 -0700

    adding colors to strokes, removing commented-out code

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -7,27 +7,15 @@ import { Context } from '../store';
 import { urls } from './utils/Constants';
 import moment from 'moment';
 
+const COLORS = ['#8884d8', '#82ca9d', '#e57cf9', '#8b2412', '#f83581', '#f07b50', '#0c5e59', '#0011ff', '#595163'];
+
 function Chart() {
 
   const { state } = useContext(Context);
 
   const [parsedData, setParsedData] = useState([]);
 
-  // //properties
-  // const data = this.props.getData;
-  // const name = this.props.setClassName;
-  // const symbols = this.props.getSymbols;
-
-  // var lines = [];
-  // var colors = ['#8884d8', '#82ca9d', '#e57cf9', '#8b2412', '#f83581', '#f07b50', '#0c5e59', '#0011ff', '#595163'];
-  // var colorIndex = 0;
-  // symbols.forEach(function (symbol) {
-  //   var color = colors[colorIndex++ % colors.length]; // rotate colors
-  //   lines.push(<Line type="monotone" key={symbol} dataKey={symbol} dot={false} unit=" USD" stroke={color} />);
-  // });
   useEffect(() => {
-    // validate info on server (check if username already exists etc.)
-
     // there are TWO actions changing state.symbols
     // a symbol is either added to the list or removed from the list
     const { symbols, end_date, start_date, budget } = state;
@@ -140,32 +128,19 @@ function Chart() {
   return (
     <Paper className="graph" style={{ width: "80vw", height: "60vh", margin: 'auto' }}>
       <ResponsiveContainer width='100%' height='100%'>
-        {/* <LineChart data={data}>
-          <XAxis dataKey="date" angle={-20} textAnchor="end" height={55} />
-          <YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }} />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          {lines}
-        </LineChart> */}
         <LineChart width={730} height={250} data={parsedData}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          {/* <XAxis dataKey="name" />
-          <YAxis /> */}
           <XAxis dataKey="date" type="category" allowDuplicatedCategory={false} angle={-20} textAnchor="end" height={55} />
           <YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }} />
           <Tooltip />
           <Legend />
           <Legend />
-          {parsedData.map(s => (
-            <Line dataKey="value" data={s.data} name={s.symbol} key={s.symbol} />
+          {parsedData.map((s, idx) => (
+            <Line dataKey="value" data={s.data} name={s.symbol} key={s.symbol} stroke={COLORS[idx % COLORS.length]}/>
           ))}
-          {/* <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
         </LineChart>
       </ResponsiveContainer>
-      {/* <p>here</p> */}
     </Paper>
   );
 }
