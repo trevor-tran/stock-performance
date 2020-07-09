41a9e8b859252d9ad2a28f3c483e8138d86dcc8d
Phuong Tran <ptran816@uw.edu>
2020-07-08 21:57:32 -0700

    handling expected urls

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,9 +1,5 @@
-import React, { useReducer } from "react";
-import {urls} from './utils/Constants'
-// import ReactDOM from "react-dom";
-// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-// import update from 'immutability-helper';
-// import moment from 'moment';
+import React, { useReducer} from "react";
+import {urls, sessions} from './utils/Constants'
 
 //routers
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
@@ -14,13 +10,18 @@ import SignupForm from './SignupForm';
 import Header from './Header';
 import Footer from './Footer';
 import GraphContainer from "./GraphContainer";
-
+import NotFound from './NotFound'
 // state management
 import { Context, initialState, reducer } from '../store'
 
 
 function Layout() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  let newState = initialState;
+  if (sessionStorage.getItem(sessions.USER_STATE)) {
+    newState = JSON.parse(sessionStorage.getItem(sessions.USER_STATE));
+  }
+
+  const [state, dispatch] = useReducer(reducer, newState);
 
   return (
     <Context.Provider value={{ state, dispatch }}>
@@ -29,14 +30,20 @@ function Layout() {
         <Switch>
           {/* https://github.com/ReactTraining/react-router/issues/4105#issuecomment-291834881 */}
           {/* an alternative way for component is render={() => <SigninForm/>} */}
-          <Route exact path={urls.SIGNIN} component={SigninForm} />
-          <Route exact path={urls.SIGNUP} component={SignupForm} />
-          <Route exact path={urls.GRAPH} render={() => (
-          state.is_authenticated ? (<GraphContainer/>) : (alert("Please log in!"), (<Redirect to={urls.SIGNIN}/>))
+          {/* <Route exact path={urls.SIGNIN} component={SigninForm} /> */}
+
+          <Route exact path={urls.SIGNIN} render={() => (
+            state.is_authenticated ? <Redirect to={urls.GRAPH}/> : <SigninForm/>
           )}/>
-          <Route path="*" render={() => (
-            <Redirect to= {urls.SIGNIN} />
-          )} />
+          <Route exact path={urls.SIGNUP} render={() => (
+            state.is_authenticated ? <Redirect to={urls.GRAPH}/> : <SignupForm/>
+          )}/>
+          <Route exact path={urls.GRAPH} render={() => (
+          state.is_authenticated ? <GraphContainer/> : <Redirect to={urls.SIGNIN}/>
+          )}/>
+          <Route path="*" render={() => {
+            return <NotFound/>
+          }} />
         </Switch>
         <Footer/>
       </Router>
