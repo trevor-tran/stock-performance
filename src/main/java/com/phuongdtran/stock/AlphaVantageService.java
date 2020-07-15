e19b6e718c70fb7717702007c451527e5839b59e
Phuong Tran <ptran816@uw.edu>
2020-07-14 19:45:58 -0700

    removed url printing

--- src/main/java/com/phuongdtran/stock/AlphaVantageService.java
+++ src/main/java/com/phuongdtran/stock/AlphaVantageService.java
@@ -97,7 +97,6 @@ public class AlphaVantageService implements IStockService {
                 .setQueryParameter("outputsize", size)
                 .setQueryParameter("apikey", ALPHA_VANTAGE_KEY)
                 .build();
-        System.out.println(url.toString());
         return url;
     }
 
