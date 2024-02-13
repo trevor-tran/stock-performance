f1cd10eee739d36c75a94703c7430ad80d6661eb
Trevor Tran <fuongdtran@gmail.com>
2024-02-13 11:46:57 -0800

    init commit

---
+++ src/main/java/com/trevor/stockperformance/StockPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.trevor.stockperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevor/stockperformance/StockPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevor.stockperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
