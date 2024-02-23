bbd5b14fbb72e5a8dfe104ac81b3fb4025a26b5f
Trevor Tran <fuongdtran@gmail.com>
2024-02-23 15:24:54 -0800

    add sign up, sign in pages

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Chart from './components/Chart';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/>
+      <Chart /> */}
+      <SignUp/>
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Chart.js
@@ -0,0 +1,76 @@
+import { useEffect, useContext, useState } from "react";
+import { LineChart, ResponsiveContainer, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+import {Paper} from '@mui/material';
+
+
+const COLORS = ['#8884d8', '#82ca9d', '#e57cf9', '#8b2412', '#f83581', '#f07b50', '#0c5e59', '#0011ff', '#595163'];
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+function Chart(props) {
+  return (
+    <>
+      <Paper className="graph" style={{ width: "80vw", height: "60vh", margin: 'auto' }}>
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart width={730} height={250} data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" type="category" allowDuplicatedCategory={false} angle={-20} textAnchor="end" height={55} />
+            <YAxis label={{ value: 'U.S. dollars ($)', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Legend />
+            <Legend />
+              <Line  dataKey="uv" stroke={COLORS[0]} dot={false} />
+              <Line  dataKey="pv" stroke={COLORS[1]} dot={false} />
+          </LineChart>
+        </ResponsiveContainer>
+      </Paper>
+    </>
+  );
+}
+
+export default Chart;
