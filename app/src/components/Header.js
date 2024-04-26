774b4c1a371dceba7e69a70da4a1213e1d2d1ac3
Trevor Tran <fuongdtran@gmail.com>
2024-04-25 17:35:47 -0700

    add donation button

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,38 +1,60 @@
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Tooltip, } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 
+import "../assets/css/Header.css";
+
 export default function Header(props) {
   return (
     <Box className="col-12 col-xl-10 col-xxl-8 m-auto">
-      <header className="d-flex flex-column flex-sm-row flex-wrap flex-wrap justify-content-center justify-content-sm-between align-items-center">
+      <header className="d-flex flex-column flex-sm-row flex-wrap justify-content-center justify-content-sm-between align-items-center">
         <a href="/" className="d-flex align-items-center text-decoration-none">
-          <span className="fs-4 fw-bold">Stock Comparator <sup className="fs-6">beta</sup></span>
+          <span className="fs-2 fw-bold">Stock Comparator <sup className="fs-6">beta</sup></span>
         </a>
-        <ul className="nav ms-sm-auto col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-0 ms-md-3 ms-lg-2">
-            <a href="https://www.linkedin.com/in/trevorpt/" target="_blank" rel="noreferrer">
-              <IconButton aria-label="linkedin">
-                <LinkedInIcon />
-              </IconButton>
+        <Box className="d-flex flex-column flex-sm-row justify-content-between align-items-center">
+          <Tooltip title="Consider donating to support our work">
+            <a
+              className="donation-button btn me-0 me-sm-2 me-md-4 order-1 order-sm-0"
+              style={{ textDecoration: "none", height: "45px" }}
+              href="https://www.paypal.com/donate/?business=MXF7G339EFAJ2&no_recurring=0&item_name=Please+consider+supporting+our+efforts+with+a+donation.+Your+contribution+will+help+us+cover+the+costs+of+maintaining+the+app.&currency_code=USD"
+              role="button"
+              target="_blank" rel="noreferrer">
+              <img src={process.env.PUBLIC_URL + '/donation.png'} alt="PayPal" />
+              <span className="d-none d-md-inline" style={{ color: "white", fontWeight: "bold", fontSize: "20px", marginLeft: "7px" }}>Donate</span>
             </a>
-          </li>
-          <li className="ms-0 ms-md-3 ms-lg-2">
-            <a href="https://github.com/trevor-tran" target="_blank" rel="noreferrer">
-              <IconButton aria-label="github">
-                <GitHubIcon />
-              </IconButton>
-            </a>
-          </li>
-          <li className="ms-0 ms-md-3 ms-lg-2">
-            <a href="mailto:trevor.phuong.tran@gmail.com">
-              <IconButton aria-label="github">
-                <EmailIcon />
-              </IconButton>
-            </a>
-          </li>
-        </ul>
+          </Tooltip>
+          <div class="d-none d-sm-block ms-md-1 vr" style={{ height: "35px", verticalAlign: "center", margin: "auto" }}></div>
+          <ul className="nav list-unstyled ms-0 ms-sm-2 ms-md-3">
+            <li className="ms-0">
+              <Tooltip title="LinkedIn">
+                <a href="https://www.linkedin.com/in/trevorpt/" target="_blank" rel="noreferrer">
+                  <IconButton aria-label="linkedin">
+                    <LinkedInIcon fontSize="large" />
+                  </IconButton>
+                </a>
+              </Tooltip>
+            </li>
+            <li className="ms-0 ms-md-1">
+              <Tooltip title="GitHub">
+                <a href="https://github.com/trevor-tran" target="_blank" rel="noreferrer">
+                  <IconButton aria-label="github">
+                    <GitHubIcon fontSize="large" />
+                  </IconButton>
+                </a>
+              </Tooltip>
+            </li>
+            <li className="ms-0 ms-md-1">
+              <Tooltip title="Email">
+                <a href="mailto:trevor.phuong.tran@gmail.com">
+                  <IconButton aria-label="github">
+                    <EmailIcon fontSize="large" />
+                  </IconButton>
+                </a>
+              </Tooltip>
+            </li>
+          </ul>
+        </Box>
       </header>
     </Box>
   );
